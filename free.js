cec10c87c86a6b88d95ff938c127ab0a7f5259a4
ChanellVS <zariachanellb@gmail.com>
2025-03-18 18:22:08 -0400

    Complete

--- free.js
+++ free.js
@@ -1,15 +1,27 @@
 console.dir(document)
 
 const freelancers = [
-    {name: "Alice" , occupation: "Writer" , price: "$30"},
-    {name: "Bob", occupation: "Teacher", price: "$50"},
+        {name: "Alice" , 
+        occupation: "Writer" , 
+        price: "30"},
+        {name: "Bob", 
+        occupation: "Teacher", 
+        price: "50"},
 ];
 
 
-const newFree =[
-    {name: 'Carol' , occupation: "Programmer" , price: "$70"}
+// const newFree =[
+//     {name: 'Carol' , occupation: "Programmer" , price: "70"},
+
+// ]
+
+const arrNam = [
+    "Phil", 'Jess'
 ]
 
+const arrJobs = [
+    "Doctor", 'Rocket Scientist'
+]
 
 console.dir(freelancers)
 
@@ -33,6 +45,7 @@ par.innerHTML = "";
 root.append(par);
 par.style.border = "1px, solid, black";
 
+
 // -------------------------------------------------------------------------------------
 const title2 =document.createElement("h2");
 title2.innerHTML = "Available Freelancers";
@@ -40,10 +53,11 @@ par.append(title2);
 console.log(par);
 // -------------------------------------------------------------------------------------
 const div = document.createElement("div");
+div.id ="start"
 div.innerHTML = "<span id='person'><strong>Name</strong></span> <span id='job'><strong>Occupation</strong></span> <span id='strPri'><strong>Price</strong></span>";
 par.append(div);
 
-renderPeo()
+// renderPeo()
 // -----------------------------------------------------------------------------
 
 const person = document.querySelector('#person');
@@ -59,44 +73,99 @@ const strPri = document.querySelector('#strPri');
 strPri.style.marginInline = "25px";
 strPri.style.fontSize = "20px"
 // -------------------------------------------------------------------------------------
+function average (){
+const sum = freelancers.reduce((acc, curr) => parseInt(acc) + parseInt(curr.price), 0);
+const priceAve = sum / freelancers.length
+return (priceAve).toFixed(2)
+}
+
+function renderAve(){
+    const aveDiv = document.getElementById("average");
+    if(aveDiv){
+        aveDiv.innerHTML = `<h2>The average starting price: ${average()} </h2>`;
+    } else{
+    const div2 = document.createElement("div");
+    div2.id = "average";
+    div2.innerHTML = `<h2>The average starting price: ${average()} </h2>`;
+    par.append(div2);
+    }
+    }
+
 function renderPeo(){
-freelancers.forEach((e)=>{
-    const cont1 = document.querySelector('#person')
-   const namDiv = document.createElement("div");
-   namDiv.classList.add("people");
-   namDiv.innerHTML = `${e.name}`;
-   cont1.append(namDiv);
+    const start = document.getElementById('start');
+    start.innerHTML = "<span id='person'><strong>Name</strong></span> <span id='job'><strong>Occupation</strong></span> <span id='strPri'><strong>Price</strong></span>";
 
-   const cont2 = document.querySelector('#job')
-   const ocDiv = document.createElement("div");
-   ocDiv.classList.add("occ");
-   ocDiv.innerHTML = `${e.occupation}`;
-   cont2.append(ocDiv);
 
-   const cont3 = document.querySelector('#strPri')
-   const prDiv = document.createElement("div");
-   prDiv.classList.add("startPrice");
-   prDiv.innerHTML = `${e.price}`;
-   cont3.append(prDiv);
+    freelancers.forEach((e)=>{
+       const cont1 = document.querySelector('#person')
+       const namDiv = document.createElement("div");
+       namDiv.classList.add("people");
+       namDiv.innerHTML = `${e.name}`;
+       cont1.append(namDiv);
+    
+       const cont2 = document.querySelector('#job')
+       const ocDiv = document.createElement("div");
+       ocDiv.classList.add("occ");
+       ocDiv.innerHTML = `${e.occupation}`;
+       cont2.append(ocDiv);
+    
+       const cont3 = document.querySelector('#strPri')
+       const prDiv = document.createElement("div");
+       prDiv.classList.add("startPrice");
+       prDiv.innerHTML = `${e.price}`;
+       cont3.append(prDiv);
+    
+       cont1.style.display= "inline-block";
+       cont2.style.display= "inline-block";
+       cont3.style.display= "inline-block";
+       
+    })
+   renderAve()
+    }
 
-   cont1.style.display= "inline-block";
-   cont2.style.display= "inline-block";
-   cont3.style.display= "inline-block";
-})
+    // function addCarol(){
+    //     for(let i =0; i< newFree.length; i++){
+    //         const carolPer = newFree[i];
+    //         return freelancers.push(carolPer)
+            
+    //     }
+    // }
+
+function randomFree(){
+    const name = arrNam[Math.round(Math.random() * (arrNam.length - 1))];
+    const job = arrJobs[Math.round(Math.random() * (arrJobs.length - 1))];
+    const ranPrice = `${Math.round(Math.random() * 100)}`;
+    return {name, occupation: job, price: ranPrice}
 }
+ 
 
 
-const addFree = () => { 
-    const newFrees = newFree[0]
-    freelancers.push(newFrees)
-  
+
+
+
+function renderNewPeo(){
+   freelancers.push(randomFree());
+   renderPeo()
 }
+renderPeo()
 
-const add = setInterval(addFree, 5000)
+
+
+
+
+
+
+const add = setInterval(renderNewPeo, 5000)
 
 setTimeout(()=>{
   clearInterval(add)
-}, 10000)
+}, 30000)
+
+
+
+
+
+
 // par.innerHTML = "<span>Name</span> <span>Occupation</span> <span>Price</span>";
 // par.innerHTML = "<span>Occupation</span>";
 // par.innerHTML = "<span>Price</span>";
